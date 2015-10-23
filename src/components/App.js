f5b9b49a1fd32ab7da7651d6223b0f066eba5463
Tonypai <tony77794@gmail.com>
2015-10-23 13:59:22 +0800

    Extract clickHandler to Pager component

--- src/components/App.js
+++ src/components/App.js
@@ -2,37 +2,24 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 import Pager from "./Pager";
-import { fetchPost, prevPage, nextPage } from "../actions/PagerAction";
+import { fetchPost } from "../actions/PagerAction";
 
 class App extends Component {
-	constructor(props) {
-		super(props);
-		this.handleClick = this.handleClick.bind(this);
-	}
-	handleClick(amt) {
-		const { dispatch, nowPage } = this.props;
-		if (amt === 1) {
-			dispatch(nextPage(nowPage));
-		}
-		else if (amt === -1) {
-			dispatch(prevPage(nowPage));
-		}
-	}
 	componentWillReceiveProps(nextProps) {
-		if (nextProps.nowPage !== this.props.nowPage) {
-			const { dispatch, nowPage } = nextProps;
-			dispatch(fetchPost(nowPage));
+		const { dispatch, postDetail } = nextProps;
+		if (nextProps.postDetail.nowPage !== postDetail.nowPage) {
+			dispatch(fetchPost(postDetail.nowPage));
 		}
 	}
 	componentDidMount() {
-		const { dispatch, nowPage } = this.props;
-		dispatch(fetchPost(nowPage));
+		const { dispatch, postDetail } = this.props;
+		dispatch(fetchPost(postDetail.nowPage));
 	}
 	render() {
-		const { post, nowPage } = this.props;
+		const { dispatch, postDetail } = this.props;
 		return (
 			<div>
-				<Pager post={post} nowPage={nowPage} onClick={this.handleClick} />
+				<Pager postDetail={postDetail} dispatch={dispatch} />
 			</div>
 		);
 	}
@@ -40,8 +27,10 @@ class App extends Component {
 
 function mapStateToProps(state) {
 	return {
-		nowPage: state.nowPage,
-		post: state.post,
+		postDetail: {
+			nowPage: state.nowPage,
+			post: state.post			
+		}
 	};
 }
 
--- src/components/Pager.js
+++ src/components/Pager.js
@@ -1,24 +1,41 @@
 import React, { Component, PropTypes } from "react";
+import { prevPage, nextPage } from "../actions/PagerAction";
 
 class Pager extends Component {
+	constructor(props) {
+		super(props);
+		this.handleNextPage = this.handleNextPage.bind(this);
+		this.handlePrevPage = this.handlePrevPage.bind(this);
+	}
+	handleNextPage() {
+		const { dispatch, postDetail } = this.props;
+		dispatch(nextPage(postDetail.nowPage));
+	}
+	handlePrevPage() {
+		const { dispatch, postDetail } = this.props;
+		dispatch(prevPage(postDetail.nowPage));
+	}
 	render() {
-		const { post, nowPage, onClick } = this.props;
+		const { postDetail, onClick } = this.props;
 		return (
 			<div>
-				Page: {nowPage}
+				Page: {postDetail.nowPage}
 				{' '}
-				<button onClick={e => onClick(-1)}>Prev</button>
+				<button onClick={this.handlePrevPage}>Prev</button>
 				{' '}
-				<button onClick={e => onClick(1)}>Next</button>
-				<h1>{post.title}</h1>
-				<p>{post.body}</p>
+				<button onClick={this.handleNextPage}>Next</button>
+				<h1>{postDetail.post.title}</h1>
+				<p>{postDetail.post.body}</p>
 			</div>
 		);
 	}
 }
 
 Pager.propTypes = {
-	nowPage: PropTypes.number.isRequired
+	postDetail: PropTypes.shape({
+		nowPage: PropTypes.number.isRequired,
+		post: PropTypes.object.isRequired
+	})
 };
 
 export default Pager;
