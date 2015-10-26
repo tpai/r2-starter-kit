3b695537724cd9bf05888b7f826229d591168c40
Tonypai <tony77794@gmail.com>
2015-10-26 15:08:55 +0800

    Give to Caesar what belongs to caesar

--- src/components/App.js
+++ src/components/App.js
@@ -2,36 +2,15 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 import Pager from "./Pager";
-import { fetchPost } from "../actions/PagerAction";
 
 class App extends Component {
-	componentWillReceiveProps(nextProps) {
-		const { dispatch, postDetail } = this.props;
-		if (nextProps.postDetail.nowPage !== postDetail.nowPage) {
-			dispatch(fetchPost(postDetail.nowPage));
-		}
-	}
-	componentDidMount() {
-		const { dispatch, postDetail } = this.props;
-		dispatch(fetchPost(postDetail.nowPage));
-	}
 	render() {
-		const { dispatch, postDetail } = this.props;
 		return (
 			<div>
-				<Pager postDetail={postDetail} dispatch={dispatch} />
+				<Pager />
 			</div>
 		);
 	}
 }
 
-function mapStateToProps(state) {
-	return {
-		postDetail: {
-			nowPage: state.nowPage,
-			post: state.post			
-		}
-	};
-}
-
-export default connect(mapStateToProps)(App);
+export default connect()(App);
--- src/components/Pager.js
+++ src/components/Pager.js
@@ -1,5 +1,6 @@
 import React, { Component, PropTypes } from "react";
-import { prevPage, nextPage } from "../actions/PagerAction";
+import { connect } from "react-redux";
+import { prevPage, nextPage, fetchPost } from "../actions/PagerAction";
 
 class Pager extends Component {
 	constructor(props) {
@@ -7,35 +8,54 @@ class Pager extends Component {
 		this.handleNextPage = this.handleNextPage.bind(this);
 		this.handlePrevPage = this.handlePrevPage.bind(this);
 	}
+	componentWillReceiveProps(nextProps) {
+		const { dispatch, detail } = this.props;
+		if (nextProps.detail.nowPage !== detail.nowPage) {
+			dispatch(fetchPost(detail.nowPage));
+		}
+	}
+	componentDidMount() {
+		const { dispatch, detail } = this.props;
+		dispatch(fetchPost(detail.nowPage));
+	}
 	handleNextPage() {
-		const { dispatch, postDetail } = this.props;
-		dispatch(nextPage(postDetail.nowPage));
+		const { dispatch, detail } = this.props;
+		dispatch(nextPage(detail.nowPage));
 	}
 	handlePrevPage() {
-		const { dispatch, postDetail } = this.props;
-		dispatch(prevPage(postDetail.nowPage));
+		const { dispatch, detail } = this.props;
+		dispatch(prevPage(detail.nowPage));
 	}
 	render() {
-		const { postDetail } = this.props;
+		const { detail } = this.props;
 		return (
 			<div>
-				Page: {postDetail.nowPage}
+				Page: {detail.nowPage}
 				{' '}
 				<button onClick={this.handlePrevPage}>Prev</button>
 				{' '}
 				<button onClick={this.handleNextPage}>Next</button>
-				<h1>{postDetail.post.title}</h1>
-				<p>{postDetail.post.body}</p>
+				<h1>{detail.post.title}</h1>
+				<p>{detail.post.body}</p>
 			</div>
 		);
 	}
 }
 
 Pager.propTypes = {
-	postDetail: PropTypes.shape({
+	detail: PropTypes.shape({
 		nowPage: PropTypes.number.isRequired,
 		post: PropTypes.object.isRequired
 	})
 };
 
-export default Pager;
+function mapStateToProps(state) {
+	return {
+		detail: {
+			nowPage: state.nowPage,
+			post: state.post			
+		}
+	};
+}
+
+export default connect(mapStateToProps)(Pager);
