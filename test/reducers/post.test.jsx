5a2502fb0f934d3ac0971701982e376b470c72b4
Tonypai <tony77794@gmail.com>
2015-11-26 18:13:53 +0800

    Rename directory name and improving reducer tests

---
+++ test/reducers/post.test.jsx
@@ -0,0 +1,48 @@
+import expect from "expect";
+import deepFreeze from "deep-freeze";
+import { UPDATE_PATH } from "redux-simple-router";
+
+import * as reducers from "src/reducers/post";
+import * as actions from "src/actions/post";
+
+describe("Reducer::Post", () => {
+	it("#post(undefined, {})", () => {
+		expect(
+			reducers.post(undefined, {})
+		).toEqual({ id: -1, title: "Now", body: "Loading..." })
+	})
+	it("#post({...}, {type: RECEIVE_POST})",  () => {
+		const beforeState = {};
+		const action = {
+			type: actions.RECEIVE_POST,
+			post: { id: 1, title: "yo", body: "man" }
+		};
+		const afterState = { id: 1, title: "yo", body: "man" };
+		
+		deepFreeze(beforeState);
+		deepFreeze(action);
+		
+		expect(
+			reducers.post(beforeState, action)
+		).toEqual(
+			afterState
+		)
+	})
+	it("#post({...}, {type: UPDATE_PATH})",  () => {
+		const beforeState = {};
+		const action = {
+			type: UPDATE_PATH,
+			post: { id: 1, title: "yo", body: "man" }
+		};
+		const afterState = { id: 1, title: "yo", body: "man" }; 
+		
+		deepFreeze(beforeState);
+		deepFreeze(action);
+		
+		expect(
+			reducers.post(beforeState, action)
+		).toEqual(
+			afterState
+		)
+	})
+})
