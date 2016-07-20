8214fa6dd0f476d5ea281c517b282c2fc154b67a
tonypai <tony77794@gmail.com>
2016-07-20 12:07:11 +0800

    Fix express server

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
+var fallback = require('express-history-api-fallback');
 var path = require('path');
 var express = require('express');
 var app = express();
 var http = require('http').createServer(app);
 var port = process.env.PORT || 8080;
 
-app.use(express.static(path.resolve(__dirname, 'www/')));
+var root = path.resolve(__dirname, './www');
+app.use(express.static(root));
+app.use(fallback('index.html', { root: root }));
+
+app.get('*', function (request, response){
+    response.sendFile(path.resolve(__dirname, 'www', 'index.html'))
+});
 
 http.listen(port, function() {
-    console.log("Now Listening http://localhost:" + port);
+    console.log("Listening http://localhost:" + port);
 });
