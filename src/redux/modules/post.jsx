fdc163683080f1d5f6422053c5ee4657c6ab7c75
tonypai <tony77794@gmail.com>
2016-07-28 18:59:38 +0800

    Add failed type and test

--- src/redux/modules/post.jsx
+++ src/redux/modules/post.jsx
@@ -1,4 +1,7 @@
+import _ from 'lodash';
+
 export const GET = 'app/post/GET';
+export const FAILED = 'app/post/FAILED';
 
 const reducer = (state = {
     title: '',
@@ -8,6 +11,12 @@ const reducer = (state = {
     switch (action.type) {
         case GET:
             return _.assign({}, state, action.post);
+        case FAILED:
+            return {
+                title: 'Fetch failed...',
+                author: { name: 'null', email: 'null'},
+                body: 'null'
+            };
         default:
             return state;
     }
@@ -17,12 +26,17 @@ export default reducer;
 
 export const getPost = (id, cb = () => {}) => {
     return async dispatch => {
-        const postRes = await fetch(`http://jsonplaceholder.typicode.com/posts/${id}`);
-        const post = await postRes.json();
-        const userRes = await fetch(`http://jsonplaceholder.typicode.com/users/${post.userId}`);
-        const user = await userRes.json();
-        cb();
-        dispatch(gotPost(_.assign({}, post, { author: user })));
+        try {
+            const postRes = await fetch(`http://jsonplaceholder.typicode.com/posts/${id}`);
+            const post = await postRes.json();
+            const userRes = await fetch(`http://jsonplaceholder.typicode.com/users/${post.userId}`);
+            const user = await userRes.json();
+            dispatch(gotPost(_.assign({}, post, { author: user })));
+            cb();
+        } catch(err) {
+            dispatch(failGetPost());
+            cb();
+        }
     }
 }
 
@@ -30,5 +44,11 @@ export const gotPost = json => {
     return {
         type: GET,
         post: json
+    }
+}
+
+export const failGetPost = () => {
+    return {
+        type: FAILED
     }
 }
