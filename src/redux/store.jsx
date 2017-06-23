c902f204ec4cf7fdc12e1b0f78466ae97f855a53
tonypai <tony77794@gmail.com>
2017-06-23 11:18:39 +0800

    Migrating redux-logger

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -2,7 +2,7 @@
 
 import { compose, createStore, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
-import createLogger from 'redux-logger';
+import { createLogger } from 'redux-logger'
 
 import reducers from 'redux/reducer';
 
