a2d2d0fb3a81293e6ab0c9d08e4fc8fcf6dd6a0d
tonypai <tony77794@gmail.com>
2017-06-23 11:19:32 +0800

    Migrating react proptypes

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import Breadcrumb from 'components/Breadcrumb';
 
 export default function Content({ title, author, body }) {
