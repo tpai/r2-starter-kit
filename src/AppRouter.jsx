0c6c6593ade96e0161a536d498eeed118ca95423
Tony Pai <tony77794@gmail.com>
2017-12-26 18:42:41 +0800

    Use dynamic import instead of static elements

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,25 +1,82 @@
-import React from 'react';
-import {
-    BrowserRouter as Router,
-    Route
-} from 'react-router-dom';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import ArticleList from 'containers/ArticleList';
-import ArticleContent from 'containers/ArticleContent';
+function asyncComponent(importComponent) {
+  class AsyncComponent extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        component: null
+      };
+    }
+    async componentDidMount() {
+      const { default: component } = await importComponent();
+      this.setState({
+        component: component
+      });
+    }
+    render() {
+      const C = this.state.component;
+      return C ? <C {...this.props} /> : null;
+    }
+  }
+  return AsyncComponent;
+}
+
+class Routes extends Component {
+  constructor() {
+    super();
+    this.routes= [{
+      name: 'List',
+      path: '/',
+      exact: true
+    }, {
+      name: 'Content',
+      path: '/article/:id'
+    }].map(route => ({
+      ...route,
+      component: () => asyncComponent(() => import(`./containers/${route.name}`))
+    }));
+  }
+  static contextName = '__routes__';
+  static childContextTypes = {
+    [Routes.contextName]: PropTypes.arrayOf(PropTypes.object)
+  };
+  getChildContext() {
+    return {
+      [Routes.contextName]: this.routes
+    };
+  }
+  renderRoutes() {
+    return this.routes.map(route => this.renderRoute(route));
+  }
+  renderRoute({ name, path, exact, component }) {
+    if (path) {
+      return <Route
+        key={name}
+        path={path}
+        exact={exact}
+        component={component}
+      />;
+    } else {
+      return <div key="404">404 not found</div>;
+    }
+  }
+  render() {
+    return (
+      <Switch>
+        {this.renderRoutes()}
+      </Switch>
+    );
+  }
+}
 
 function AppRouter() {
     return (
         <Router>
             <div className="ts text container" style={{ margin: '1rem' }}>
-                <Route
-                  exact
-                  path="/"
-                  component={ArticleList}
-                />
-                <Route
-                  path="/article/:id"
-                  component={ArticleContent}
-                />
+              <Routes />
             </div>
         </Router>
     );
