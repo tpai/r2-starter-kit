04a5b8edde8f6d62ad4d05e85930f80d44ceabd6
Tony Pai <tony77794@gmail.com>
2018-01-10 14:58:35 +0800

    Add postcss-import sample

--- src/index.jsx
+++ src/index.jsx
@@ -6,8 +6,8 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 
 import AppRouter from 'AppRouter';
-
 import store from 'redux/store';
+import 'index.css';
 
 render(
     <Provider store={store}>
