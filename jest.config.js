615cdea42b5db42e639fea2dcf297276df0a8c15
tonypai <tony77794@gmail.com>
2018-02-26 02:02:25 +0800

    Add jasmine-reporters package
    Support code coverage report in development mode, you could find report at `tests/__coverage__/`.

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
-  setupFiles: ["./jest.setup.js"],
-  snapshotSerializers: ["enzyme-to-json/serializer"]
+  setupFiles: ['./jest.setup.js'],
+  snapshotSerializers: ['enzyme-to-json/serializer'],
+  collectCoverage: true,
+  coverageReporters: ['json', 'html'],
+  coverageDirectory: '<rootDir>/tests/__coverage__/',
+  setupTestFrameworkScriptFile: '<rootDir>/setup-jasmine-env.js'
 };
---
+++ setup-jasmine-env.js
@@ -0,0 +1,9 @@
+jasmine.VERBOSE = true;
+
+var reporters = require('jasmine-reporters');
+var junitReporter = new reporters.JUnitXmlReporter({
+  savePath: 'output/',
+  consolidateAll: false
+});
+
+jasmine.getEnv().addReporter(junitReporter);
