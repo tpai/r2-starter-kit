1a3e6e5e18fe2528af6517f551327f599ae82c58
Tony Pai <tony77794@gmail.com>
2018-12-11 16:09:32 +0800

    Fix e2e test

--- tests/e2e.js
+++ tests/e2e.js
@@ -1,31 +1,39 @@
 /* global fixture, window */
 
 import { ClientFunction, Selector } from 'testcafe';
+import { waitForReact, ReactSelector } from 'testcafe-react-selectors';
 
-fixture`e2e test`.page`http://localhost:8080/`;
+fixture`post list`
+  .page`http://localhost:8080/`
+  .beforeEach(async () => await waitForReact());
 
-const getPageUrl = ClientFunction(() => window.location.href);
-
-test('list page', async (t) => {
+test('render Items', async (t) => {
   await t
-    .expect(Selector('.item').count).eql(20)
+    .expect(ReactSelector('Items').count).eql(1)
     .wait(1000);
 });
 
-test('detail page', async (t) => {
+test('click post', async (t) => {
   await t
-    .click(Selector('.header').nth(0))
+    .click(Selector('.item').nth(0))
     .expect(Selector('.image').count).eql(1)
     .expect(Selector('.content').count).eql(1)
     .expect(Selector('.description').count).eql(1)
     .wait(1000);
 });
 
-test('go to detail page and back', async (t) => {
+fixture`post`.page`http://localhost:8080/posts/1`;
+
+test('render Item', async t => {
+  await t.expect(ReactSelector('Item').count).eql(1)
+    .wait(1000);
+});
+
+test('click breadcrumb `post list`', async (t) => {
+  const getPageUrl = ClientFunction(() => window.location.href);
+
   await t
-    .click(Selector('.header').nth(0))
-    .expect(getPageUrl()).contains('http://localhost:8080/post/1')
-    .click(Selector('.breadcrumb a').nth(0))
+    .click(ReactSelector('Breadcrumb').findReact('a').nth(0))
     .expect(getPageUrl()).contains('http://localhost:8080/')
     .wait(1000);
 });
