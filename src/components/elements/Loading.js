4298dc72e7e3bbb9caa68c0d81f82bdc1d3ca7d6
Tony Pai <tony77794@gmail.com>
2018-12-12 12:06:18 +0800

    Use double quotes string instead of template string

--- src/components/elements/Loading.js
+++ src/components/elements/Loading.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Loading() {
   return (
     <div className="ts active inverted dimmer">
-      <div className={`ts text loader`}>Loading...</div>
+      <div className="ts text loader">Loading...</div>
     </div>
   );
 }
