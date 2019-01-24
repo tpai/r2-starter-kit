8f345a93d338ef7fb37411ee5e21110a0542a3e8
Tony Pai <tony77794@gmail.com>
2019-01-24 18:08:03 +0800

    Move Loading component into folder and fix style

--- src/components/elements/Loading/index.js
+++ src/components/elements/Loading/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import cx from 'classnames';
+
+import styles from './styles.scss';
 
 export default function Loading() {
   return (
-    <div className="ts active inverted dimmer">
+    <div className={cx('ts', 'active', 'inverted', 'dimmer', styles.container)}>
       <div className="ts text loader">Loading...</div>
     </div>
   );
