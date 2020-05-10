e4732945121eb750b65c105540acfeba3af921dc
Tony Pai <tony77794@gmail.com>
2020-05-11 02:04:51 +0800

    Use testing-library instead enzyme

--- setup-jasmine-env.js
+++
@@ -1,9 +0,0 @@
-jasmine.VERBOSE = true;
-
-var reporters = require('jasmine-reporters');
-var junitReporter = new reporters.JUnitXmlReporter({
-  savePath: 'tests/__jenkins__/',
-  consolidateAll: false
-});
-
-jasmine.getEnv().addReporter(junitReporter);
