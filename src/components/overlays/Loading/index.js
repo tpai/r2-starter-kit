d255324e431634e2f4524f50877f630cdc36e39d
Tony Pai <tony77794@gmail.com>
2020-05-11 13:15:00 +0800

    Lock bodyscroll when loading

--- src/components/overlays/Loading/index.js
+++ src/components/overlays/Loading/index.js
@@ -1,6 +1,11 @@
 import React, { useRef, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
+import {
+  clearAllBodyScrollLocks,
+  disableBodyScroll,
+  enableBodyScroll,
+} from 'body-scroll-lock';
 
 import styles from './styles.module.scss';
 
@@ -15,6 +20,17 @@ export default function Loading({ isLoading }) {
       root.removeChild(el.current);
     };
   }, []);
+
+  useEffect(() => {
+    if (isLoading) {
+      disableBodyScroll(el);
+    } else {
+      enableBodyScroll(el);
+    }
+    return () => {
+      clearAllBodyScrollLocks();
+    };
+  }, [isLoading]);
 
   return ReactDOM.createPortal(
     isLoading ?
