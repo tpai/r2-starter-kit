5728d3a6b9ee3c25b20c86bd5d5342afe6423c47
Tony Pai <tony77794@gmail.com>
2020-05-11 14:17:18 +0800

    Add e2e test

---
+++ src/e2e.js
@@ -0,0 +1,22 @@
+/* global fixture */
+
+import { ClientFunction, Selector } from 'testcafe';
+import { waitForReact, ReactSelector } from 'testcafe-react-selectors';
+
+fixture`repo list`
+  .page`http://localhost:8080/`
+  .beforeEach(async () => await waitForReact());
+
+test('initial render', async t => {
+  await t.expect(ReactSelector('Repos').count).eql(1);
+});
+
+const scrollToBottom = ClientFunction(function() {
+  window.scrollBy(0, window.document.body.offsetHeight);
+});
+
+test('infinite load', async t => {
+  await t.expect(Selector('.item').count).eql(15);
+  await scrollToBottom();
+  await t.expect(Selector('.item').count).eql(20);
+});
